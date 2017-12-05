bac19ac5873e2c07bb12f4e15e8e1bfe770d5c29
ZhouLiMing <719630857@qq.com>
2017-12-05 14:52:41 +0800

    Bean转化为xml字符串功能提交

--- src/test/java/com/github/gongfuboy/utils/BeanUtilsTest.java
+++ src/test/java/com/github/gongfuboy/utils/BeanUtilsTest.java
@@ -1,5 +1,10 @@
 package com.github.gongfuboy.utils;
 
+import com.github.gongfuboy.utils.enums.TransformEnum;
+import com.github.gongfuboy.utils.pojo.Human;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * BeanUtils测试类
  *
@@ -9,4 +14,17 @@ package com.github.gongfuboy.utils;
  */
 public class BeanUtilsTest {
 
+    private Human human;
+
+    @Before
+    public void init() {
+        human = new Human();
+        human.setName("name");
+        human.setAge("age");
+    }
+
+    @Test
+    public void testTransformBeanToXml() throws NoSuchFieldException, IllegalAccessException {
+        System.out.println(BeanUtils.transformBeanToXml(human, TransformEnum.CAMAL_CASE));
+    }
 }
---
+++ src/test/java/com/github/gongfuboy/utils/pojo/Human.java
@@ -0,0 +1,29 @@
+package com.github.gongfuboy.utils.pojo;
+
+/**
+ * @author GongFuBoy
+ * @date 2017/12/5
+ * @time 14:49
+ */
+public class Human {
+
+    private String name;
+
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
