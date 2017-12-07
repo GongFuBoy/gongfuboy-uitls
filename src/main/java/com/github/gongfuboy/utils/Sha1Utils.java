6cf5faaa198bcab4fb4559aa24b806a32d656293
ZhouLiMing <719630857@qq.com>
2017-12-07 16:04:18 +0800

    SHA1加密工具

--- src/main/java/com/github/gongfuboy/utils/Sha1Utils.java
+++ src/main/java/com/github/gongfuboy/utils/Sha1Utils.java
@@ -17,6 +17,40 @@ import java.util.Map;
  */
 public class Sha1Utils {
 
+    // 每一个键值对之间的连接符
+    private String connection;
+
+    // 键值之间的连接符
+    private String keyValueConnection;
+
+    // 单例工具
+    private static Sha1Utils sha1Utils;
+
+    /**
+     * 私有化构造方法
+     */
+    private Sha1Utils() {
+
+    }
+
+    /**
+     * 单例构造方法
+     * @param connection
+     * @return
+     */
+    public static Sha1Utils instance(String connection, String keyValueConnection) {
+        if (sha1Utils == null) {
+            synchronized (Sha1Utils.class) {
+                if (sha1Utils == null) {
+                    sha1Utils = new Sha1Utils();
+                    sha1Utils.connection = connection;
+                    sha1Utils.keyValueConnection = keyValueConnection;
+                }
+            }
+        }
+        return sha1Utils;
+    }
+
     /**
      * SHA1 安全加密算法
      *
@@ -24,7 +58,7 @@ public class Sha1Utils {
      * @return
      * @throws DigestException
      */
-    public static String SHA1(Map<String, Object> maps) throws DigestException {
+    public String SHA1(Map<String, Object> maps) throws DigestException {
         //获取信息摘要 - 参数字典排序后字符串
         String decrypt = getOrderByLexicographic(maps);
         try {
@@ -57,7 +91,7 @@ public class Sha1Utils {
      * @param maps 参数key-value map集合
      * @return String 排序后的字符串
      */
-    private static String getOrderByLexicographic(Map<String, Object> maps) {
+    private String getOrderByLexicographic(Map<String, Object> maps) {
         return splitParams(lexicographicOrder(getParamsName(maps)), maps);
     }
 
@@ -67,7 +101,7 @@ public class Sha1Utils {
      * @param maps 参数key-value map集合
      * @return
      */
-    private static List<String> getParamsName(Map<String, Object> maps) {
+    private List<String> getParamsName(Map<String, Object> maps) {
         List<String> paramNames = new ArrayList<String>();
         for (Map.Entry<String, Object> entry : maps.entrySet()) {
             paramNames.add(entry.getKey());
@@ -81,7 +115,7 @@ public class Sha1Utils {
      * @param paramNames 参数名称List集合
      * @return 排序后的参数名称List集合
      */
-    private static List<String> lexicographicOrder(List<String> paramNames) {
+    private List<String> lexicographicOrder(List<String> paramNames) {
         Collections.sort(paramNames);
         return paramNames;
     }
@@ -93,17 +127,21 @@ public class Sha1Utils {
      * @param maps       参数key-value map集合
      * @return String 拼接后的字符串
      */
-    private static String splitParams(List<String> paramNames, Map<String, Object> maps) {
+    private String splitParams(List<String> paramNames, Map<String, Object> maps) {
         StringBuilder paramStr = new StringBuilder();
-        for (String paramName : paramNames) {
-            paramStr.append(paramName);
+        for (int i=0; i < paramNames.size(); i++) {
+            paramStr.append(paramNames.get(i)).append(keyValueConnection);
             for (Map.Entry<String, Object> entry : maps.entrySet()) {
-                if (paramName.equals(entry.getKey())) {
+                if (paramNames.get(i).equals(entry.getKey())) {
                     paramStr.append(String.valueOf(entry.getValue()));
                 }
             }
+            if (i == paramNames.size() - 1) {
+                paramStr.append("");
+            } else {
+                paramStr.append(connection);
+            }
         }
         return paramStr.toString();
-
     }
 }
--- src/test/java/com/github/gongfuboy/utils/RandomUtilsTest.java
+++ src/test/java/com/github/gongfuboy/utils/RandomUtilsTest.java
@@ -1,6 +1,9 @@
 package com.github.gongfuboy.utils;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Test;
+
+import java.text.ParseException;
 
 /**
  * RandomUtils测试类
@@ -12,7 +15,10 @@ import org.junit.Test;
 public class RandomUtilsTest {
 
     @Test
-    public void testCreateRondomString() {
-        System.out.println(RandomUtils.createRondomString(6, 0, 10));
+    public void testCreateRondomString() throws ParseException {
+        //1512612625000
+        //15126126250
+        String s = String.valueOf(DateUtils.parseDate("2017-12-07 10:10:25", "yyyy-MM-dd hh:mm:ss").getTime());
+        System.out.println(s.substring(0, 10));
     }
 }
