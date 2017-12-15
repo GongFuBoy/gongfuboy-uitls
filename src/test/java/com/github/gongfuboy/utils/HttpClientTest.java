730f6df5316d4e26cecba88511c8eba4852f5cc4
ZhouLiMing <719630857@qq.com>
2017-12-15 10:14:42 +0800

    注释当前httpClient代码

--- src/test/java/com/github/gongfuboy/utils/HttpClientTest.java
+++ src/test/java/com/github/gongfuboy/utils/HttpClientTest.java
@@ -1,17 +1,6 @@
 package com.github.gongfuboy.utils;
 
-import com.github.gongfuboy.utils.wechat.Constants;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 /**
  * @author GongFuBoy
@@ -22,18 +11,18 @@ public class HttpClientTest {
 
     @org.junit.Test
     public void test() throws IOException {
-        StringBuilder stringBuilder = new StringBuilder();
-        HttpGet httpGet = new HttpGet("http://localhost:8080/washcar/wechatquery/queryIndexInfo?mobile=17674130318");
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        HttpResponse execute = httpClient.execute(httpGet);
-        HttpEntity entity = execute.getEntity();
-        InputStream content = entity.getContent();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content, Constants.ENCODING));
-        String line = bufferedReader.readLine();
-        while (StringUtils.isNotBlank(line)) {
-            stringBuilder.append(line);
-            line = bufferedReader.readLine();
-        }
-        System.out.println(stringBuilder.toString());
+//        StringBuilder stringBuilder = new StringBuilder();
+//        HttpGet httpGet = new HttpGet("http://localhost:8080/washcar/wechatquery/queryIndexInfo?mobile=17674130318");
+//        HttpClient httpClient = HttpClientBuilder.create().build();
+//        HttpResponse execute = httpClient.execute(httpGet);
+//        HttpEntity entity = execute.getEntity();
+//        InputStream content = entity.getContent();
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content, Constants.ENCODING));
+//        String line = bufferedReader.readLine();
+//        while (StringUtils.isNotBlank(line)) {
+//            stringBuilder.append(line);
+//            line = bufferedReader.readLine();
+//        }
+//        System.out.println(stringBuilder.toString());
     }
 }
