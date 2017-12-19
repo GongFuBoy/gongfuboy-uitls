c85b69f361a9c3818ae3a4beb66018c18c1b08e7
ZhouLiMing <719630857@qq.com>
2017-12-19 17:50:49 +0800

    项目介绍

--- readme.md
+++ readme.md
@@ -3,3 +3,15 @@
 1. 概述
 
     这是GongFuBoy平时留下的一些工具代码，目前维护人只有GongFuBoy一人
+    
+2. 代码包结构简介
+
+|包结构路径|简介|
+|---|--|
+|com.github.gongfuboy.uitls|项目根目录|
+|com.github.gongfuboy.uitls.annotation|项目存放注解类路径|
+|com.github.gongfuboy.uitls.enums|整个工程存放枚举路径|
+|com.github.gongfuboy.uitls.wechat|微信支付等工具路径|
+|com.github.gongfuboy.uitls.wechat.common|微信工具公共包|
+|com.github.gongfuboy.uitls.wechat.openid|微信获取openid包路径|
+|com.github.gongfuboy.uitls.wechat.recharge|微信微信支付包路径|
--- src/test/java/com/github/gongfuboy/utils/HttpClientTest.java
+++ src/test/java/com/github/gongfuboy/utils/HttpClientTest.java
@@ -1,6 +1,18 @@
 package com.github.gongfuboy.utils;
 
+import com.github.gongfuboy.utils.wechat.Constants;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * @author GongFuBoy
@@ -11,18 +23,19 @@ public class HttpClientTest {
 
     @org.junit.Test
     public void test() throws IOException {
-//        StringBuilder stringBuilder = new StringBuilder();
-//        HttpGet httpGet = new HttpGet("http://localhost:8080/washcar/wechatquery/queryIndexInfo?mobile=17674130318");
-//        HttpClient httpClient = HttpClientBuilder.create().build();
-//        HttpResponse execute = httpClient.execute(httpGet);
-//        HttpEntity entity = execute.getEntity();
-//        InputStream content = entity.getContent();
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content, Constants.ENCODING));
-//        String line = bufferedReader.readLine();
-//        while (StringUtils.isNotBlank(line)) {
-//            stringBuilder.append(line);
-//            line = bufferedReader.readLine();
-//        }
-//        System.out.println(stringBuilder.toString());
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpGet httpGet = new HttpGet("http://localhost:8080/washcar/wechatquery/queryIndexInfo?mobile=17674130318");
+        HttpPost httpPost = new HttpPost();
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpResponse execute = httpClient.execute(httpGet);
+        HttpEntity entity = execute.getEntity();
+        InputStream content = entity.getContent();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content, Constants.ENCODING));
+        String line = bufferedReader.readLine();
+        while (StringUtils.isNotBlank(line)) {
+            stringBuilder.append(line);
+            line = bufferedReader.readLine();
+        }
+        System.out.println(stringBuilder.toString());
     }
 }
